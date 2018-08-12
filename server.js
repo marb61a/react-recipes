bc4edc085079580cf672a381bc3d6471d741c06e
marb61a <marb61a@gmail.com>
2018-08-12 11:40:57 +0100

    updated server and setup mLab

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config({
+  path: 'variables.env'
+});
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {console.log('DB Connected')})
+  .catch(err => console.error(err));
+
+const app = express();
+
+const PORT = process.env.PORT || 4444;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
