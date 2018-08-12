8a9d5bc1c0460ce20972f5ae1b1ca478ecd54a58
marb61a <marb61a@gmail.com>
2018-08-12 21:07:36 +0100

    added graphql middleware to server.js

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 require('dotenv').config({
   path: 'variables.env'
+});
+
+// Brings in the GraphQL - Express middleware
+const { graphiqlExpress, graphqlExpress} = require('apollo-server-express');
+const { makeExecutableSchema } = require('graphql-tools');
+
+const Recipe = require('./models/Recipe');
+const User = require('./models/User');
+
+// GraphQL needs its own schemas
+const { typeDefs } = require('./schema');
+const { resolvers } = require('./resolvers');
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
 });
 
 mongoose
@@ -10,6 +26,20 @@ mongoose
   .catch(err => console.error(err));
 
 const app = express();
+
+// Create graphiql application
+app.use('/graphiql', graphiqlExpress({
+  endpointURL: '/graphql'
+}));
+
+// Connect mongoose schemas to graphql
+app.use('graphql', graphqlExpress({
+  schema,
+  context: {
+    Recipe,
+    User
+  }
+}));
 
 const PORT = process.env.PORT || 4444;
 
