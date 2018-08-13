471bb8d4df97441e5fc44bbbf45d4fda0dea2220
marb61a <marb61a@gmail.com>
2018-08-13 21:20:57 +0100

    added signupUser mutation

--- resolvers.js
+++ resolvers.js
@@ -1,3 +1,12 @@
+import jwt from 'jsonwebtoken';
+import { create } from 'domain';
+
+const createToken = (user, secret, expiresIn) => {
+  const { username, email } = user;
+
+  return jwt.sign({ username, email }, secret, { expiresIn });
+}
+
 exports.resolvers = {
   Query: {
     getAllRecipes: async(root, args, { Recipe }) => {
@@ -21,6 +30,21 @@ exports.resolvers = {
       }).save();
 
       return newRecipe;
+    },
+
+    signupUser: async (root, {username, email, password}, { User }) => {
+      const user = await User.findOne({username});
+      if(user){
+        throw new Error('User already exists');
+      }
+
+      const newUser = await new User({
+        username,
+        email,
+        password
+      }).save();
+
+      return {token: createToken(newUser, process.env.SECRET, '1hr')};
     }
   }
 };
