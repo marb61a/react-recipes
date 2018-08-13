007f04eb52468b5835ac60ab3bacba1fe79d4bb4
marb61a <marb61a@gmail.com>
2018-08-13 11:55:11 +0100

    installed frontend client using create-react-app

--- resolvers.js
+++ resolvers.js
@@ -2,6 +2,8 @@ exports.resolvers = {
   Query: {
     getAllRecipes: async(root, args, { Recipe }) => {
       const allRecipes = await Recipe.find();
+
+      return allRecipes;
     }
   },
   Mutation: {
