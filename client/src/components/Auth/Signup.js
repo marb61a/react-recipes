77f73aff158e4aff8a103d551c9b83d37c3f85df
marb61a <marb61a@gmail.com>
2018-08-14 20:49:40 +0100

    added signup form validation

--- client/src/components/Auth/Signup.js
+++ client/src/components/Auth/Signup.js
@@ -2,14 +2,25 @@ import React from 'react';
 import { Mutation } from 'react-apollo';
 
 import {SIGNUP_USER} from '../../queries';
+import Error from '../Error';
+
+const initialState = {
+  username: "",
+  email: "",
+  password: "",
+  passwordConfirmation: ""
+};
 
 class Signup extends React.Component {
   state = {
-    username: "",
-    email: "",
-    password: "",
-    passwordConfirmation: ""
+    ...initialState
   };
+
+  clearState = () => {
+    this.setState({
+      ...initialState
+    });
+  }
 
   handleChange = event => {
     const {name, value } = event.target;
@@ -25,7 +36,17 @@ class Signup extends React.Component {
     signupUser()
       .then(data => {
         console.log(data);
+
+        this.clearState();
       });
+  }
+
+  validateForm = () => {
+    const {username, email, password, passwordConfirmation} = this.state;
+
+    const isInvalid  = !username || !email || !password || password !== passwordConfirmation;
+
+    return isInvalid;
   }
 
   render() {
@@ -72,9 +93,14 @@ class Signup extends React.Component {
                   value={passwordConfirmation}
                   onChange={this.handleChange}
                 />
-                <button type="submit" className="button-primary">
+                <button 
+                  type="submit" 
+                  disabled={loading || this.validateForm()}
+                  className="button-primary"
+                >
                   Submit
                 </button>
+                {error && <Error error={error}/>}
               </form>
             );
           }}
