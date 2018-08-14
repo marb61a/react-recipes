d9cd8ceee9a4e467a61f5bbd5805a53ca8b6d649
marb61a <marb61a@gmail.com>
2018-08-14 13:09:21 +0100

    Updated signup.js and added error component

--- client/src/components/Auth/Signup.js
+++ client/src/components/Auth/Signup.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Mutation } from 'react-apollo';
 
+import {SIGNUP_USER} from '../../queries';
+
 class Signup extends React.Component {
   state = {
     username: "",
@@ -17,16 +19,32 @@ class Signup extends React.Component {
     });
   }
 
+  handleSubmit = (event, signupUser) => {
+    event.preventDefault();
+
+    signupUser()
+      .then(data => {
+        console.log(data);
+      });
+  }
+
   render() {
     const { username, email, password, passwordConfirmation } = this.state;
 
     return (
       <div className="App">
         <h2 className="App">Signup</h2>
-        <Mutation mutation={SIGNUP_USER}>
-          {() => {
+        <Mutation mutation={SIGNUP_USER} variables={{
+          username,
+          email,
+          password
+        }} >
+          {(signupUser, {data, loading, error}) => {
             return (
-              <form className="form">
+              <form 
+                className="form" 
+                onSubmit={event => this.handleSubmit(event, signupUser)}
+              >
                 <input 
                   type="text" 
                   name="username" 
