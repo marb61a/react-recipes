c7754cf5f8e23fde0ccf8a77d61730af69426568
marb61a <marb61a@gmail.com>
2018-08-14 12:21:29 +0100

    Updated signup.js and added SIGNUP_USER mutation

--- client/src/components/Auth/Signup.js
+++ client/src/components/Auth/Signup.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Mutation } from 'react-apollo';
 
 class Signup extends React.Component {
   state = {
@@ -8,43 +9,59 @@ class Signup extends React.Component {
     passwordConfirmation: ""
   };
 
-  handleChange = () => {
+  handleChange = event => {
+    const {name, value } = event.target;
 
+    this.setState({
+      [name]: value
+    });
   }
 
   render() {
+    const { username, email, password, passwordConfirmation } = this.state;
+
     return (
       <div className="App">
         <h2 className="App">Signup</h2>
-        <form className="form">
-          <input 
-            type="text" 
-            name="username" 
-            placeholder="Username"
-            onChange={this.handleChange}
-          />
-          <input 
-            type="email" 
-            name="email" 
-            placeholder="Email Address"
-            onChange={this.handleChange}
-          />
-          <input 
-            type="password" 
-            name="password" 
-            placeholder="Password"
-            onChange={this.handleChange}
-          />
-          <input 
-            type="password" 
-            name="passwordConfirmation" 
-            placeholder="Confirm Password"
-            onChange={this.handleChange}
-          />
-          <button type="submit" className="button-primary">
-            Submit
-          </button>
-        </form>
+        <Mutation mutation={SIGNUP_USER}>
+          {() => {
+            return (
+              <form className="form">
+                <input 
+                  type="text" 
+                  name="username" 
+                  placeholder="Username"
+                  value={username}
+                  onChange={this.handleChange}
+                />
+                <input 
+                  type="email" 
+                  name="email" 
+                  placeholder="Email Address"
+                  onChange={this.handleChange}
+                />
+                <input 
+                  type="password" 
+                  name="password" 
+                  placeholder="Password"
+                  value={password}
+                  onChange={this.handleChange}
+                />
+                <input 
+                  type="password" 
+                  name="passwordConfirmation" 
+                  placeholder="Confirm Password"
+                  value={passwordConfirmation}
+                  onChange={this.handleChange}
+                />
+                <button type="submit" className="button-primary">
+                  Submit
+                </button>
+              </form>
+            );
+          }}
+          
+        </Mutation>
       </div>
     );
   }
