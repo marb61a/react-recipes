c75b5d8b471084f578831bbcbb6f7125ec3f00cd
marb61a <marb61a@gmail.com>
2018-08-16 20:51:34 +0100

    converted AddRecipe to a stateful component

--- client/src/components/Recipe/AddRecipe.js
+++ client/src/components/Recipe/AddRecipe.js
@@ -1,30 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
+import { Mutation } from "react-apollo";
 
-const AddRecipe = () => (
-  <div className="App">
-    <h2 className="App">Add Recipe</h2>
-    <form className="form">
-      <input
-        type="text"
-        name="name"
-        placeholder="Add Name"
-        onChange={this.handleChange}
-      />
-      <select name="category" onChange={this.handleChange}>
-        <option value="Breakfast">Breakfast</option>
-        <option value="Lunch">Lunch</option>
-        <option value="Dinner">Dinner</option>
-        <option value="Snack">Snack</option>
-      </select>
-      <input
-        type="text"
-        name="description"
-        placeholder="Add Description"
-        onChange={this.handleChange}
-      />
-    </form>
-  </div>
-);
+import { ADD_RECIPE, GET_ALL_RECIPES, GET_USER_RECIPES } from "../../queries";
+import Error from "../Error";
+
+const initialState = {
+  name: "",
+  imageUrl: "",
+  instructions: "",
+  category: "Breakfast",
+  description: "",
+  username: ""
+};
+
+class AddRecipe extends Component{
+  state = { ...initialState };
+
+  clearState = () => {
+    this.setState({ ...initialState });
+  };
+
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  render(){
+    return(
+    <div className="App">
+      <h2 className="App">Add Recipe</h2>
+      <form className="form">
+        <input
+          type="text"
+          name="name"
+          placeholder="Add Name"
+          onChange={this.handleChange}
+        />
+        <select name="category" onChange={this.handleChange}>
+          <option value="Breakfast">Breakfast</option>
+          <option value="Lunch">Lunch</option>
+          <option value="Dinner">Dinner</option>
+          <option value="Snack">Snack</option>
+        </select>
+        <input
+          type="text"
+          name="description"
+          placeholder="Add Description"
+          onChange={this.handleChange}
+        />
+      </form>
+    </div>
+    )
+  }
+};
 
 export default withRouter(AddRecipe);
