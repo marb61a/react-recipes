7c69d5f282ddc7834206af66a8c69ff6cbd9b3ea
marb61a <marb61a@gmail.com>
2018-08-17 11:26:24 +0100

    added query caching to AddRecipe

--- client/src/components/Recipe/AddRecipe.js
+++ client/src/components/Recipe/AddRecipe.js
@@ -26,6 +26,27 @@ class AddRecipe extends Component{
     this.setState({ [name]: value });
   };
 
+  validateForm = () => {
+    const { 
+      name, imageUrl, category, description, instructions 
+    } = this.state;
+    const isInvalid =
+      !name || !imageUrl || !category || !description || !instructions;
+    
+    return isInvalid;
+  };
+
+  updateCache = (cache, {data: { addRecipe }}) => {
+    const { getAllRecipes } = cache.readQuery({ query: GET_ALL_RECIPES });
+
+    cache.writeQuery({
+      query: GET_ALL_RECIPES,
+      data: {
+        getAllRecipes: [addRecipe, ...getAllRecipes]
+      }    
+    })
+  }
+
   render(){
     const {
       name,
@@ -51,19 +72,25 @@ class AddRecipe extends Component{
         {(addRecipe, { data, loading, error }) => 
           <div className="App">
             <h2 className="App">Add Recipe</h2>
-            <form className="form">
+            <form className="form"
+              className="form"
+              onSubmit={event => this.handleSubmit(event, addRecipe)}
+            >
+              <label htmlFor="name">Recipe Name</label>
               <input
                 type="text"
                 name="name"
                 placeholder="Add Name"
                 onChange={this.handleChange}
               />
+              <label htmlFor="category">Category of Recipe</label>
               <select name="category" onChange={this.handleChange}>
                 <option value="Breakfast">Breakfast</option>
                 <option value="Lunch">Lunch</option>
                 <option value="Dinner">Dinner</option>
                 <option value="Snack">Snack</option>
               </select>
+              <label htmlFor="description">Recipe Description</label>
               <input
                 type="text"
                 name="description"
