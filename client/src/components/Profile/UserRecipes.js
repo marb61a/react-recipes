a098c4e405db3d749d4ad9bebd4c4d833e2f2293
marb61a <marb61a@gmail.com>
2018-08-19 12:10:13 +0100

    Updated SearchItem and added initial UserRecipes

---
+++ client/src/components/Profile/UserRecipes.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Query, Mutation } from "react-apollo";
+import Spinner from "../Spinner";
+
+import {
+  GET_USER_RECIPES,
+  DELETE_USER_RECIPE,
+  GET_ALL_RECIPES,
+  GET_CURRENT_USER
+} from "../../queries";
+
+
+const UserRecipes = ({ username }) => (
+  <Query>
+  
+  </Query>
+);
---
+++ client/src/components/Recipe/SearchItem.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const SearchItem = ({ _id, name, }) => (
+  <li>
+  
+  </li>
+);
+
+export default SearchItem;
