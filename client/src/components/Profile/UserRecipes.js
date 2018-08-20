4bc25cbb37d9b4cee11ecc074e9268ff1b4caf98
marb61a <marb61a@gmail.com>
2018-08-20 11:58:09 +0100

    updated UserRecipes

--- client/src/components/Profile/UserRecipes.js
+++ client/src/components/Profile/UserRecipes.js
@@ -10,9 +10,39 @@ import {
   GET_CURRENT_USER
 } from "../../queries";
 
+const handleDelete = deleteUserRecipe => {
+  const confirmDelete = window.confirm(
+    "Are you sure you want to delete this recipe?"
+  );
+
+  if(confirmDelete){
+    deleteUserRecipe()
+      .then(({data}) => {
+        console.log(data);
+      });
+  }
+};
 
 const UserRecipes = ({ username }) => (
-  <Query>
-  
+  <Query query={GET_USER_RECIPES} variables={{ username }}>
+    {
+      ({data, loading, error}) => {
+        if (loading) return <Spinner />;
+        if (error) return <div>Error</div>;
+
+        return(
+          <ul>
+            <h3>Your Recipes</h3>
+            {
+              !data.getUserRecipes.length && (
+                <p>
+                  <strong>You have not added any recipes yet</strong>
+                </p>
+              )
+            }
+          </ul>
+        )
+      }
+    }
   </Query>
 );
