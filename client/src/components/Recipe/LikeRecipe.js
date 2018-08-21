2b29ee08242c921e810826249a73c9bda24ccede
marb61a <marb61a@gmail.com>
2018-08-21 16:11:51 +0100

    added handleClick and handleLike methods to LikeRecipe

--- client/src/components/Recipe/LikeRecipe.js
+++ client/src/components/Recipe/LikeRecipe.js
@@ -13,9 +13,35 @@ class LikeRecipe extends Component {
   componentDidMount(){
     if(this.props.session.getCurrentUser){
       const { username, favorites } = this.props.session.getCurrentUser;
+      const { _id } = this.props;
+      const prevLiked = favorites.findIndex(favorite => favorite._id === _id) > -1;
+
+      this.setState({
+        liked: prevLiked,
+        username
+      });
+    }
+  };
+
+  handleClick = (likeRecipe, unlikeRecipe) => {
+    this.setState(
+      prevState => ({
+        liked: !prevState.liked
+      }),
+      () => this.handleLike(likeRecipe, unlikeRecipe)
+    )
+  };
+
+  handleLike = (likeRecipe, unlikeRecipe) => {
+    if(this.state.liked){
+      likeRecipe()
+        .then(async ({ data }) => {
+          await this.props.refetch();
+        });
+    } else {
 
     }
-  }
+  };
 
   render(){
     const { liked, username } = this.state;
